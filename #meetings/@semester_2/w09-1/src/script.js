0f303319a19cde27fe7e0cd83b38db1225ef6d2e
Mihail Petrov <mail@mihail-petrov.me>
2021-12-06 10:53:08 +0200

    w09-1 into to type script

---
+++ #meetings/@semester_2/w09-1/src/script.js
@@ -0,0 +1,13 @@
+// 0. JS
+// ##
+// const HELLO_WORLD = "Hello World";
+// const printHelloWorld = () => {
+//     return HELLO_WORLD;
+// }
+// 1. Types
+// ##
+var numberVariable;
+numberVariable = 5;
+numberVariable = 10;
+// 2. classes / abstract
+// 3. interfaces
